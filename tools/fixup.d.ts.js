da99bdda89e02fba6030a9abac5e65cffcf40493
Gregg Tavares <github@greggman.com>
2023-07-21 10:22:10 -0700

    add fixup for .d.ts

---
+++ tools/fixup.d.ts.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const readTextFile = n => fs.readFileSync(n, {encoding: 'utf-8'});
+
+const pkg = JSON.parse(readTextFile(path.join(__dirname, '..', 'package.json')));
+const majorVersion = pkg.version.split('.')[0];
+
+const dtsFilename = path.join(__dirname, '..', 'dist', `${majorVersion}.x`, 'buffer-views.d.ts');
+const s = readTextFile(dtsFilename)
+const newS = s.replace(
+  /export\s+type\s+ArrayBufferViews\s+=\s+{\s+views:\s+TypedArrayOrViews;/m,
+  `export type ArrayBufferViews = {
+    views: any; // because otherwise this is too much of a PITA to use in typescript`);
+console.assert(s !== newS, `fixup failed: did you build first?\n`);
+fs.writeFileSync(dtsFilename, newS);
