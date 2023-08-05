ba0a971bade421f5c1bdca2bc72065f6cfd1d565
Gregg Tavares <github@greggman.com>
2023-08-05 12:58:53 -0700

    stop including dist/docs/index.html

--- dist/0.x/webgpu-utils.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './buffer-views.js';
-export * from './data-definitions.js';
-export * from './generate-mipmap.js';
-export * from './attribute-utils.js';
-export * from './texture-utils.js';
-export * from './typed-arrays.js';
