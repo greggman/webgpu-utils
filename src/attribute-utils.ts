587a2f7ccf1ff82c2e6b2c1028d3b51a54d64d1b
Gregg Tavares <github@greggman.com>
2023-08-06 20:23:20 -0700

    just 'indices'

--- src/attribute-utils.ts
+++ src/attribute-utils.ts
@@ -40,7 +40,7 @@ export type ArrayUnion = number | number[] | TypedArray | FullArraySpec;
  *
  * Each array can be 1 of 4 things. A native JavaScript array, a TypedArray, a number, a {@link FullArraySpec}
  *
- * If it's a native array then, if the name of the array is `indices`, `index` or `ndx` the data will be converted
+ * If it's a native array then, if the name of the array is `indices` the data will be converted
  * to a `Uint32Array`, otherwise a `Float32Array.  Use a TypedArray or a FullArraySpec to choose a different type.
  * The FullArraySpec type is only used if it's not already a TypedArray
  *
@@ -82,7 +82,7 @@ export type BuffersAndAttributes = {
 };
 
 function isIndices(name: string) {
-  return name === "indices" || name === 'index' || name === 'ndx';
+  return name === "indices";
 }
 
 function makeTypedArrayFromArrayUnion(array: ArrayUnion, name: string): TypedArray {
@@ -407,7 +407,7 @@ export function interleaveVertexData(
  * * `buffers` will have one `GPUBuffer` of usage `GPUBufferUsage.VERTEX`
  * * `indexBuffer` will be `GPUBuffer` of usage `GPUBufferUsage.INDEX`
  * * `indexFormat` will be `uint32` (use a full spec or a typedarray of `Uint16Array` if you want 16bit indices)
- * * `numElements` will be 36 (this is either the number entries in the array named `indices`, `index` or `ndx` or if no
+ * * `numElements` will be 36 (this is either the number entries in the array named `indices` or if no
  *    indices are provided then it's the length of the first array divided by numComponents. See {@link Arrays})
  *
  * See {@link Arrays} for details on the various types of arrays.
